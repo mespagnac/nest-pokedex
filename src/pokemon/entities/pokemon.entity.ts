d21c6750ca25610e9f5424d37ee9cc7954ac8ed8
mespagnac <ricardomaespagnac@gmail.com>
2023-01-27 01:13:06 -0300

    Fin de seccion 7

---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,24 @@
+import { Schema } from "@nestjs/mongoose";
+import { Prop, SchemaFactory } from "@nestjs/mongoose/dist";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document{
+    
+    //id: string // Mongo me lo da
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: number;
+
+}
+
+
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon );
